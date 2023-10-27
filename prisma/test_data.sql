e57a62e0a739105aee6ddba08573eff16de81a55
Mager <andrew.mager@gmail.com>
2023-10-27 14:19:32 -0500

    Migration stuff

---
+++ prisma/test_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    "Dataset" (id, userId, name, slug, source, description)
+VALUES
+    (
+        'clo7mxjld00013alq90ows91i',
+        'clo09oga800003altg1inpq4b',
+        'Illinois',
+        'illinois',
+        'https://github.com/mager/maps/tree/main/illinois' 'My first dataset! A simple map of Illinois with a blue gradient.',
+    );
