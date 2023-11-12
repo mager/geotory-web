5d1767c682fc17946812d8c18a910a28e80127e1
Mager <andrew.mager@gmail.com>
2023-11-12 11:36:49 -0600

    node 21

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "vercel.com", "avatars.githubusercontent.com", "raw.githubusercontent.com"],
+    remotePatterns: [{
+      protocol: "https",
+      "hostname": "lh3.googleusercontent.com"
+    }, {
+      protocol: "https",
+      "hostname": "vercel.com"
+    }, {
+      protocol: "https",
+      "hostname": "avatars.githubusercontent.com"
+    }, {
+      protocol: "https",
+      "hostname": "raw.githubusercontent.com"
+    }],
   },
   async redirects() {
     return [
